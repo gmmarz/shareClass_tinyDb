55e54623842c261eb07a409b8597aba3c468df2d
gmmarz <guilhermemarzano@gmail.com>
2023-11-05 10:57:59 -0300

    Second commit

--- ShClass_tinyDb_aula.py
+++ ShClass_tinyDb_aula.py
@@ -2,10 +2,11 @@
 from tinydb import TinyDB
 
 #Para criar o banco de dados.
-db = TinyDB('./databse.json')
+db = TinyDB('./databse.json', indent = 2)
 
 #Criando um documento 
 document = {'name':'Davi','altura':1.75}
 
 #Inserir informações no banco
 db.insert(document)
+
